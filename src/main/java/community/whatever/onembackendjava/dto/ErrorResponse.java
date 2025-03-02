1cf3542f9cb01057ecd92350caf02f74c62c41f8
Sung-Heon <elon4856@gmail.com>
2025-03-02 15:07:43 +0900

    [refactor] ZonedDateTime으로 변경.

--- src/main/java/community/whatever/onembackendjava/dto/ErrorResponse.java
+++ src/main/java/community/whatever/onembackendjava/dto/ErrorResponse.java
@@ -1,13 +1,14 @@
 package community.whatever.onembackendjava.dto;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 public record ErrorResponse(
     String errorCode,
     String message,
-    LocalDateTime timestamp
+    ZonedDateTime timestamp
 ) {
     public static ErrorResponse of(String errorCode, String message) {
-        return new ErrorResponse(errorCode, message, LocalDateTime.now());
+        return new ErrorResponse(errorCode, message, ZonedDateTime.now(ZoneId.of("Asia/Seoul")));
     }
 }

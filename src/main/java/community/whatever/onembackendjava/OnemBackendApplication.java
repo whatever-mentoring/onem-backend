90c83ef0720b1bbfa39e051ea11873c9ffe7f101
Sung-Heon <elon4856@gmail.com>
2025-03-03 21:09:35 +0900

    [feature] 애플리케이션 전체의 기본 시간대를 Asia/Seoul로 설정 (현재 배포 instance가 Berlin 시간대로 되어있어서 추가)

--- src/main/java/community/whatever/onembackendjava/OnemBackendApplication.java
+++ src/main/java/community/whatever/onembackendjava/OnemBackendApplication.java
@@ -3,10 +3,14 @@ package community.whatever.onembackendjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OnemBackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
         SpringApplication.run(OnemBackendApplication.class, args);
     }
 }

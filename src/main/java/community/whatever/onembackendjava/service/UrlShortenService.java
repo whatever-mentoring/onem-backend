ab0d118fe63cdfc1ae78f29dc48e0790ac0e26f0
Sung-Heon <elon4856@gmail.com>
2025-03-09 12:26:21 +0900

    [feature] 밖에서 호출로 변경.

--- src/main/java/community/whatever/onembackendjava/service/UrlShortenService.java
+++ src/main/java/community/whatever/onembackendjava/service/UrlShortenService.java
@@ -26,6 +26,8 @@ public class UrlShortenService {
 
     private static final int KEY_LENGTH = 6;
     private static final long ONE_HOUR = 60 * 60 * 1000;
+    private final String envPrefix = appEnvironment.getPrefix();
+
 
     private String envPrefix;
 

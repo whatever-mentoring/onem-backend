4eec43b8fc8c7c11ba2ef3ce3fc0735a6ec22cc9
ALbertIM0427 <guang7226961@naver.com>
2025-03-23 23:35:04 +0900

    refactor: 환경 설정 오류 수정

--- src/main/kotlin/community/whatever/onembackendkotlin/application/ShortenUrlIdGeneration.kt
+++ src/main/kotlin/community/whatever/onembackendkotlin/application/ShortenUrlIdGeneration.kt
@@ -6,7 +6,9 @@ import java.util.UUID
 @Component
 class ShortenUrlIdGeneration {
     companion object {
-        private val keyPrefix: String = System.getenv("spring.profiles.active") ?: "local"
+        private val keyPrefix: String =
+            System.getenv("SPRING_PROFILES_ACTIVE")
+                ?: throw IllegalStateException("SPRING_PROFILES_ACTIVE 시스템 환경변수가 설정되지 않았습니다.")
     }
 
     fun generateId(): String {
